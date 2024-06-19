636fec8475d649410f5f3490b8cb6a6f1c345a0e
Ismayil Hasanov <ismailhasanov@gmail.com>
2024-06-19 15:30:04 +0300

    fix: firebase config for all platforms

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
 import 'login_screen.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
-
-  FirebaseOptions firebaseOptions = FirebaseOptions(
-    apiKey: dotenv.env['FIREBASE_API_KEY']!,
-    authDomain: dotenv.env['FIREBASE_AUTH_DOMAIN']!,
-    projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
-    storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET']!,
-    messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
-    appId: dotenv.env['FIREBASE_APP_ID']!,
-    measurementId: dotenv.env['FIREBASE_MEASUREMENT_ID'],
-  );
-
-  await Firebase.initializeApp(options: firebaseOptions);
-
+  await DefaultFirebaseOptions.loadEnv();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
