cf44473d8fb3ecc72495b55a2a7c8e2985af60c0
Ismayil Hasanov <ismailhasanov@gmail.com>
2024-06-25 14:50:01 +0300

    fix: main app changed back to material app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'dart:io';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'firebase_options.dart';
@@ -27,18 +25,11 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    if (Platform.isIOS) {
-      return const CupertinoApp(
-          home: SafeArea(
-        child: AuthWrapper(),
-      ));
-    } else {
-      return const MaterialApp(
-          home: SafeArea(
-        child: AuthWrapper(),
-      ));
-    }
+  Widget build(BuildContext context) {    
+    return const MaterialApp(
+        home: SafeArea(
+      child: AuthWrapper(),
+    ));
   }
 }
 
