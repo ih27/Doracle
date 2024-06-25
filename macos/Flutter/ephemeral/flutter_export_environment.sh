dd12a4c9b4f1c476f540f49f530e0b9d4c05cbe4
Ismayil Hasanov <ismailhasanov@gmail.com>
2024-06-25 10:54:54 +0300

    chore: remove unused platforms

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/ihasanov/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/ihasanov/Desktop/fortuntella"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=99.99.99"
+export "FLUTTER_BUILD_NUMBER=99"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
