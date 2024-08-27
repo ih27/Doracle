b50d0bd6a893b17534c9d8474789490ee6db79ff
Ismayil Hasanov <ismailhasanov@gmail.com>
2024-08-27 12:55:53 +0300

    fix: bond screen now has last results button

--- lib/widgets/bond_buttons.dart
+++ lib/widgets/bond_buttons.dart
@@ -10,15 +10,55 @@ class BondButtons extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
+          SizedBox(
+            width: MediaQuery.sizeOf(context).width,
+            height: 70,
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 20, 4),
+                  child: IconButton(
+                    icon: Icon(
+                      Icons.bookmark_sharp,
+                      color: Theme.of(context).primaryColor,
+                      size: 24,
+                    ),
+                    onPressed: () => print('Last Result pressed ...'),
+                    iconSize: 50,
+                    padding: EdgeInsets.zero,
+                    constraints: const BoxConstraints(
+                      minWidth: 50,
+                      minHeight: 50,
+                    ),
+                    style: IconButton.styleFrom(
+                      shape: CircleBorder(
+                        side: BorderSide(
+                          color: Theme.of(context).primaryColor,
+                          width: 2,
+                        ),
+                      ),
+                      backgroundColor: Theme.of(context).colorScheme.secondary,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            width: MediaQuery.sizeOf(context).width,
+            height: MediaQuery.sizeOf(context).height * 0.14,
+          ),
+          _buildIntroLine(context),
           _buildCompatibilityButton(
             context,
-            'You\nPet',
+            'Parent\nPet',
             'assets/images/owner_pet.png',
             () => onNavigate('/owner/compatability'),
           ),
-          const SizedBox(height: 20),
           _buildCompatibilityButton(
             context,
             'Pet\nPet',
@@ -26,6 +66,27 @@ class BondButtons extends StatelessWidget {
             () => onNavigate('/pet/compatability'),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildIntroLine(BuildContext context) {
+    return Align(
+      alignment: const AlignmentDirectional(-1, 0),
+      child: Padding(
+        padding: const EdgeInsetsDirectional.fromSTEB(25, 0, 0, 0),
+        child: Container(
+          decoration: const BoxDecoration(),
+          child: Text(
+            'Create profiles and discover matches!',
+            textAlign: TextAlign.start,
+            style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                  color: AppTheme.primaryColor,
+                  fontSize: 16,
+                  letterSpacing: 0,
+                ),
+          ),
+        ),
       ),
     );
   }
@@ -38,53 +99,56 @@ class BondButtons extends StatelessWidget {
   ) {
     return GestureDetector(
       onTap: onTap,
-      child: Container(
-        width: MediaQuery.of(context).size.width * 0.9,
-        height: 190,
-        decoration: BoxDecoration(
-          color: AppTheme.alternateColor,
-          borderRadius: BorderRadius.circular(20),
-          border: Border.all(
-            color: AppTheme.accent1,
-            width: 2,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.9,
+          height: 190,
+          decoration: BoxDecoration(
+            color: AppTheme.alternateColor,
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(
+              color: AppTheme.accent1,
+              width: 2,
+            ),
           ),
-        ),
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Align(
-              alignment: const AlignmentDirectional(-1, 0),
-              child: Padding(
-                padding: const EdgeInsetsDirectional.fromSTEB(15, 0, 0, 0),
-                child: Text(
-                  title,
-                  style: Theme.of(context).textTheme.displaySmall?.copyWith(
-                        color: AppTheme.primaryColor,
-                        letterSpacing: 0,
-                        fontWeight: FontWeight.bold,
-                      ),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Align(
+                alignment: const AlignmentDirectional(-1, 0),
+                child: Padding(
+                  padding: const EdgeInsetsDirectional.fromSTEB(15, 0, 0, 0),
+                  child: Text(
+                    title,
+                    style: Theme.of(context).textTheme.displaySmall?.copyWith(
+                          color: AppTheme.primaryColor,
+                          letterSpacing: 0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
                 ),
               ),
-            ),
-            Align(
-              alignment: const AlignmentDirectional(1, 1),
-              child: Padding(
-                padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 10, 0),
-                child: Container(
-                  width: 200,
-                  height: 170,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      fit: BoxFit.contain,
-                      alignment: Alignment.bottomCenter,
-                      image: AssetImage(imagePath),
+              Align(
+                alignment: const AlignmentDirectional(1, 1),
+                child: Padding(
+                  padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 10, 0),
+                  child: Container(
+                    width: 200,
+                    height: 170,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        fit: BoxFit.contain,
+                        alignment: Alignment.bottomCenter,
+                        image: AssetImage(imagePath),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
